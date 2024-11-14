257e00d1d16e41fcca3a5d1f2e3f697c52e87573
ocean-gravel <gavrila@tcd.ie>
2024-11-14 01:16:48 +0000

    comments + how to run

--- array.c
+++ array.c
@@ -3,6 +3,7 @@
 #include <memory.h>
 
 
+// I definitely think that I should be using _generics, that way I wont need to worry about writing functions for different data types
 bool equals(int *A, int *B,int sizeA, int sizeB) {
     printf("Size of A: %d \n", sizeA);
     printf("Size of B: %d \n", sizeB);
